6bc1e0702ccdcc5ba9ed51fe5fccd3f1fd69bf98
geo <geoc2022@gmail.com>
2024-05-09 12:46:01 -0400

    Fix indentation

--- model.py
+++ model.py
@@ -22,57 +22,57 @@ BATCH_SIZE = 16
 
 @keras.saving.register_keras_serializable()
 class TransferCNN(tf.keras.Model):
-    def __init__(self) -> None:
-        super(TransferCNN, self).__init__()
+  def __init__(self) -> None:
+    super(TransferCNN, self).__init__()
 
-        self.optimizer = Adam(learning_rate=LEARNING_RATE)
+    self.optimizer = Adam(learning_rate=LEARNING_RATE)
 
-        self.vgg = VGG19(include_top=False, weights='imagenet')
+    self.vgg = VGG19(include_top=False, weights='imagenet')
 
-        self.flatten = Flatten()
-        self.dense1 = Dense(512, activation='relu')
-        self.dropout1 = Dropout(0.3)
-        self.dense2 = Dense(256, activation='relu')
-        self.dropout2 = Dropout(0.3)
-        self.dense3 = Dense(NUM_CLASSES, activation='softmax')
+    self.flatten = Flatten()
+    self.dense1 = Dense(512, activation='relu')
+    self.dropout1 = Dropout(0.3)
+    self.dense2 = Dense(256, activation='relu')
+    self.dropout2 = Dropout(0.3)
+    self.dense3 = Dense(NUM_CLASSES, activation='softmax')
 
-    def call(self, inputs):
-        x = self.vgg(inputs)
-        x = self.flatten(x)
-        x = self.dense1(x)
-        x = self.dropout1(x)
-        x = self.dense2(x)
-        x = self.dropout2(x)
-        return self.dense3(x)
+  def call(self, inputs):
+    x = self.vgg(inputs)
+    x = self.flatten(x)
+    x = self.dense1(x)
+    x = self.dropout1(x)
+    x = self.dense2(x)
+    x = self.dropout2(x)
+    return self.dense3(x)
 
-    @staticmethod
-    def loss_fn(y, y_pred):
-        return categorical_crossentropy(y, y_pred)
+  @staticmethod
+  def loss_fn(y, y_pred):
+    return categorical_crossentropy(y, y_pred)
 
-    def train_step(self, x, y):
-        with tf.GradientTape() as tape:
-            y_pred = self.call(x)
-            loss = self.loss_fn(y, y_pred)
-        loss_value = tf.reduce_mean(loss)
-        print(f"Loss: {loss_value.numpy()}\n")
-        grads = tape.gradient(loss, self.trainable_variables)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+  def train_step(self, x, y):
+    with tf.GradientTape() as tape:
+        y_pred = self.call(x)
+        loss = self.loss_fn(y, y_pred)
+    loss_value = tf.reduce_mean(loss)
+    print(f"Loss: {loss_value.numpy()}\n")
+    grads = tape.gradient(loss, self.trainable_variables)
+    self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
 
-    def train(self, x, y):
-        dataset = tf.data.Dataset.from_tensor_slices((x, y)).shuffle(len(x)).batch(BATCH_SIZE)
+  def train(self, x, y):
+    dataset = tf.data.Dataset.from_tensor_slices((x, y)).shuffle(len(x)).batch(BATCH_SIZE)
 
-        for i in range(NUM_EPOCHS):
-            print(f"Epoch {i+1}/{NUM_EPOCHS}\n")
-            for batch_x, batch_y in dataset:
-                self.train_step(batch_x, batch_y)
+    for i in range(NUM_EPOCHS):
+        print(f"Epoch {i+1}/{NUM_EPOCHS}\n")
+        for batch_x, batch_y in dataset:
+            self.train_step(batch_x, batch_y)
 
-    def build(self, input_shape):
-        self.vgg.build(input_shape)
-        super().build(input_shape)
+  def build(self, input_shape):
+    self.vgg.build(input_shape)
+    super().build(input_shape)
 
-    @classmethod
-    def from_config(cls, config):
-        return cls()
+  @classmethod
+  def from_config(cls, config):
+    return cls()
 
 def clip_0_1(image: tf.Tensor) -> tf.Tensor:
   """
